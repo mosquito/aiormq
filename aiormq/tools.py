a7ba409a67d3cc0d2b79672a7b6fe344708e86f5
Dmitry Orlov <me@mosquito.su>
2019-03-11 00:14:06 +0300

    shield function

--- aiormq/tools.py
+++ aiormq/tools.py
@@ -1,3 +1,6 @@
+import asyncio
+from functools import wraps
+
 from yarl import URL
 
 
@@ -5,3 +8,14 @@ def censor_url(url: URL):
     if url.password is not None:
         return url.with_password('******')
     return url
+
+
+def shield(func):
+    async def awaiter(future):
+        return await future
+
+    @wraps(func)
+    def wrap(*args, **kwargs):
+        return wraps(func)(awaiter)(asyncio.shield(func(*args, **kwargs)))
+
+    return wrap
