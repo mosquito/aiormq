7fb21a057aeec3321fd2d05f38f835c17fd395fa
Dmitry Orlov <me@mosquito.su>
2019-03-25 02:06:35 +0300

    [fix] tests

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -1,9 +1,41 @@
+import asyncio
+
+import pytest
+
 from aiormq.tools import LazyCoroutine
 
 
-async def test_lazy_coroutine_simple():
+@pytest.mark.asyncio
+async def test_lazy_coroutine_coro(event_loop):
     async def foo():
+        await asyncio.sleep(0, loop=event_loop)
         return 42
+
+    bar = LazyCoroutine(foo)
+
+    assert await bar == 42
+
+
+@pytest.mark.asyncio
+async def test_lazy_coroutine_future(event_loop):
+    def foo():
+        f = event_loop.create_future()
+        event_loop.call_soon(f.set_result, 42)
+        return f
+
+    bar = LazyCoroutine(foo)
+
+    assert await bar == 42
+
+
+@pytest.mark.asyncio
+async def test_lazy_coroutine_task(event_loop):
+    def foo():
+        async def inner():
+            await asyncio.sleep(0, loop=event_loop)
+            return 42
+
+        return event_loop.create_task(inner())
 
     bar = LazyCoroutine(foo)
 
