b4e7958fd5d84895ee293354c93c7c94e1d855e0
Dmitry Orlov <me@mosquito.su>
2019-03-25 02:14:42 +0300

    [fix] use class for tools tests

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -6,37 +6,34 @@ from aiormq.tools import LazyCoroutine
 
 
 @pytest.mark.asyncio
-async def test_lazy_coroutine_coro(event_loop):
-    async def foo():
-        await asyncio.sleep(0, loop=event_loop)
-        return 42
-
-    bar = LazyCoroutine(foo)
-
-    assert await bar == 42
-
-
-@pytest.mark.asyncio
-async def test_lazy_coroutine_future(event_loop):
-    def foo():
-        f = event_loop.create_future()
-        event_loop.call_soon(f.set_result, 42)
-        return f
-
-    bar = LazyCoroutine(foo)
-
-    assert await bar == 42
-
-
-@pytest.mark.asyncio
-async def test_lazy_coroutine_task(event_loop):
-    def foo():
-        async def inner():
+class TestLazyCoroutine:
+    async def test_coro(self, event_loop):
+        async def foo():
             await asyncio.sleep(0, loop=event_loop)
             return 42
 
-        return event_loop.create_task(inner())
+        bar = LazyCoroutine(foo)
 
-    bar = LazyCoroutine(foo)
+        assert await bar == 42
 
-    assert await bar == 42
+    async def test_future(self, event_loop):
+        def foo():
+            f = event_loop.create_future()
+            event_loop.call_soon(f.set_result, 42)
+            return f
+
+        bar = LazyCoroutine(foo)
+
+        assert await bar == 42
+
+    async def test_task(self, event_loop):
+        def foo():
+            async def inner():
+                await asyncio.sleep(0, loop=event_loop)
+                return 42
+
+            return event_loop.create_task(inner())
+
+        bar = LazyCoroutine(foo)
+
+        assert await bar == 42
