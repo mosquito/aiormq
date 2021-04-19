ea3da5ee3d280338918c07d25246d67c99b61935
Dmitry Orlov <me@mosquito.su>
2021-04-19 23:31:08 +0300

    fixes

--- aiormq/types.py
+++ aiormq/types.py
@@ -5,7 +5,7 @@ from .abc import *  # noqa
 
 warnings.warn(
     "aiormq.types was deprecated and will be removed in "
-    "one of next major releases",
+    "one of next major releases. Use aiormq.abc instead.",
     category=DeprecationWarning,
     stacklevel=2,
 )
