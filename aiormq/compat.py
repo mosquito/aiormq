bcc1c34efb8d2824f4cefafd233908f6cf3b6b37
Dmitry Orlov <me@mosquito.su>
2021-09-15 20:41:54 +0300

    mypy satisfaction

--- aiormq/compat.py
+++ aiormq/compat.py
@@ -2,49 +2,49 @@ import socket
 from platform import platform
 
 
-def set_keepalive_linux(
-    sock: socket.socket, keepalive_time: int, interval: int
-) -> None:
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
-    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPIDLE, interval)
-    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPINTVL, interval)
-    sock.setsockopt(
-        socket.IPPROTO_TCP,
-        socket.TCP_KEEPCNT,
-        round(keepalive_time / interval)
-    )
-
-
-def set_keepalive_osx(
-    sock: socket.socket, keepalive_time: int, interval: int
-) -> None:
-    """ Source: https://stackoverflow.com/a/14855726 """
-
-    # scraped from /usr/include, not exported by python's socket module
-    TCP_KEEPALIVE = 0x10
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
-    sock.setsockopt(socket.IPPROTO_TCP, TCP_KEEPALIVE, interval)
-
-
-def set_keepalive_windows(
-    sock: socket.socket, keepalive_time: int, interval: int
-) -> None:
-    # Source: https://stackoverflow.com/a/31868203
-    sock.ioctl(socket.SIO_KEEPALIVE_VALS, (
-        1,
-        int(keepalive_time) * 1000,
-        int(interval) * 1000
-    ))      # NOQA
-
-
 PLATFORM = platform()
 
+
 if PLATFORM == 'Windows':
-    set_keepalive = set_keepalive_windows
+    def set_keepalive(
+        sock: socket.socket, keepalive_time: int, interval: int
+    ) -> None:
+        # Source: https://stackoverflow.com/a/31868203
+        sock.ioctl(                         # type: ignore
+            socket.SIO_KEEPALIVE_VALS,      # type: ignore
+            (
+                1,
+                int(keepalive_time) * 1000,
+                int(interval) * 1000
+            )
+        )
+
 elif PLATFORM == 'Darwin':
-    set_keepalive = set_keepalive_osx
+    def set_keepalive(
+        sock: socket.socket, keepalive_time: int, interval: int
+    ) -> None:
+        """ Source: https://stackoverflow.com/a/14855726 """
+
+        # scraped from /usr/include, not exported by python's socket module
+        TCP_KEEPALIVE = 0x10
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+        sock.setsockopt(socket.IPPROTO_TCP, TCP_KEEPALIVE, interval)
+
 elif PLATFORM == 'Linux':
-    set_keepalive = set_keepalive_linux
+    def set_keepalive(
+        sock: socket.socket, keepalive_time: int, interval: int
+    ) -> None:
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+        sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPIDLE, interval)
+        sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPINTVL, interval)
+        sock.setsockopt(
+            socket.IPPROTO_TCP,
+            socket.TCP_KEEPCNT,
+            round(keepalive_time / interval)
+        )
+
 else:
-    def set_keepalive(*_):
+    def set_keepalive(
+        sock: socket.socket, keepalive_time: int, interval: int
+    ) -> None:
         return
