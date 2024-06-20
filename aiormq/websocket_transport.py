67dddf2e08bdabf595c71aa6ccf750d3806e4ea0
Alexander Loschilov <aloschil@gmail.com>
2024-06-20 12:18:52 +0300

    CHANGE: mypy to meet requirements

--- aiormq/websocket_transport.py
+++ aiormq/websocket_transport.py
@@ -1,10 +1,15 @@
-from asyncio import streams, transports, get_event_loop, events
+from asyncio import (streams, transports, get_event_loop, events,
+                     AbstractEventLoop, StreamReaderProtocol, BaseProtocol)
 import asyncio
+from typing import Dict, Optional, Any, Tuple, Callable
 
 import aiohttp
 from yarl import URL
 
 import logging
+
+from aiormq.abc import URLorStr
+from aiohttp.client_ws import ClientWebSocketResponse
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -25,12 +30,13 @@ logger.addHandler(c_handler)
 
 class WebsocketTransport(transports.Transport):
 
-    def __init__(self, loop, protocol, url, extra=None):
+    def __init__(self,
+                 loop: AbstractEventLoop,
+                 protocol: StreamReaderProtocol,
+                 url: URLorStr,
+                 extra: Optional[Dict] = None) -> None:
 
-        if extra is None:
-            extra = {}
-        self._extra = extra
-
+        super().__init__(extra)
         self.url = url
 
         self._loop = loop
@@ -40,16 +46,15 @@ class WebsocketTransport(transports.Transport):
 
         self.task = self._loop.create_task(self.main_loop())
 
-        self.write_queue = asyncio.Queue()
-        self.read_queue = asyncio.Queue()
-        self.ws = None
+        self.write_queue: asyncio.Queue = asyncio.Queue()
+        self.read_queue: asyncio.Queue = asyncio.Queue()
 
-    async def sender(self, ws):
+    async def sender(self, ws: ClientWebSocketResponse) -> None:
         while True:
             data = await self.write_queue.get()
             await ws.send_bytes(data)
 
-    async def receiver(self, ws):
+    async def receiver(self, ws: ClientWebSocketResponse) -> None:
         async for msg in ws:
             if msg.type == aiohttp.WSMsgType.BINARY:
                 self._protocol.data_received(msg.data)
@@ -58,31 +63,31 @@ class WebsocketTransport(transports.Transport):
 
         self._protocol.eof_received()
 
-    async def main_loop(self):
+    async def main_loop(self) -> None:
         async with aiohttp.ClientSession() as session:
             async with session.ws_connect(self.url) as ws:
-                self.ws = ws
                 await asyncio.gather(
                     self.sender(ws),
                     self.receiver(ws)
                 )
 
-    def get_protocol(self):
+    def get_protocol(self) -> BaseProtocol:
         return self._protocol
 
-    def set_protocol(self, protocol):
-        return self._protocol
+    def set_protocol(self,  # type: ignore[override]
+                     protocol: StreamReaderProtocol) -> None:
+        self._protocol = protocol
 
-    def is_closing(self):
+    def is_closing(self) -> bool:
         return self._closing
 
-    def write(self, data):
+    def write(self, data: Any) -> None:
         self.write_queue.put_nowait(data)
 
-    def is_reading(self):
+    def is_reading(self) -> bool:
         return not self._paused and not self._closing
 
-    def resume_reading(self):
+    def resume_reading(self) -> None:
         if self._closing or not self._paused:
             return
         self._paused = False
@@ -92,10 +97,15 @@ class WebsocketTransport(transports.Transport):
     def close(self) -> None:
         self._closing = True
         self.task.cancel()
-        self._protocol._closed.set_result(None)
+        self._protocol.connection_lost(None)
+        # self._protocol._closed.set_result(None)
 
 
-async def create_websocket_connection(protocol_factory, url, *args, **kwargs):
+async def create_websocket_connection(protocol_factory: Callable,
+                                      url: URLorStr,
+                                      **kwargs: Dict
+                                      ) -> Tuple[WebsocketTransport,
+                                                 StreamReaderProtocol]:
     loop = get_event_loop()
     protocol = protocol_factory()
     transport = WebsocketTransport(loop, protocol, url, **kwargs)
@@ -106,20 +116,20 @@ _DEFAULT_LIMIT = 2 ** 16  # 64 KiB
 
 
 async def open_websocket_connection(url: URL,
-                                    limit=_DEFAULT_LIMIT,
-                                    *args,
-                                    **kwargs):
+                                    limit: int = _DEFAULT_LIMIT,
+                                    **kwargs: Dict
+                                    ) -> Tuple[streams.StreamReader,
+                                               streams.StreamWriter]:
     loop = events.get_running_loop()
     reader = streams.StreamReader(limit=limit, loop=loop)
     protocol = streams.StreamReaderProtocol(reader, loop=loop)
 
-    def factory():
+    def factory() -> StreamReaderProtocol:
         return protocol
 
     transport, _ = await create_websocket_connection(
         factory,
         url,
-        *args,
         **kwargs
     )
     writer = streams.StreamWriter(transport, protocol, reader, loop)
